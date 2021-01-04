db7c66423aae2d5878cf0fadb122b6e5a1cfe32f
Jha <Jha@DESKTOP-52AG544>
2021-01-04 20:56:11 +0530

    this is just the package creation  by shasi laptop

---
+++ TestAppjava/src/com/shashi/beans/Student.java
@@ -0,0 +1,10 @@
+package com.shashi.beans;
+
+public class Student {
+private int sid;
+private String sname;
+public String studentDetail(int sid, String sname) {
+	System.out.println("the student detail is "+sid+ sname);
+return sname;
+}
+}
---
+++ TestAppjava/src/com/shashi/test/Tester.java
@@ -0,0 +1,7 @@
+package com.shashi.test;
+
+public class Tester {
+public static void main(String[] args) {
+	// this is the tester class 
+}
+}
