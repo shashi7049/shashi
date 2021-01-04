ee25d08abf07c994727f9d3a479a8ad1456ea870
shashi <SHASHI@SHASHI-PC>
2021-01-04 21:07:28 +0530

    this is the changes done by the shashi desktop

--- TestAppjava/src/com/shashi/test/Tester.java
+++ TestAppjava/src/com/shashi/test/Tester.java
@@ -1,7 +1,14 @@
 package com.shashi.test;
 
+import com.shashi.beans.Student;
+
 public class Tester {
 public static void main(String[] args) {
-	// this is the tester class 
+	// this is the tester class  
+	//now i am creating the object for the student class in the devloper computer
+	Student st= new Student();
+	//  now just call the method 
+	String s=st.studentDetail(10, "shashi");
+System.out.println(s);
 }
 }
